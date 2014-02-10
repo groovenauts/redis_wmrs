f41682b69636af111600f8925dc11d6d98cf9daa
akima <akm2000@gmail.com>
2014-02-10 22:14:43 +0900

    put down failed host's priority in candidate list for slave

--- lib/redis_wmrs/slave_client.rb
+++ lib/redis_wmrs/slave_client.rb
@@ -10,7 +10,18 @@ module RedisWmrs
       if sentinel?
         auto_retry_with_timeout do
           discover_slave
-          connect_without_sentinel
+          begin
+            connect_without_sentinel
+          rescue => e
+            @failed ||= []
+            f = "#{@options[:host]}:#{@options[:port]}"
+            @failed.delete(f)
+            @failed.push(f) # 必ず末尾に追加する
+            puts "@failed: #{@failed.inspect}"
+            raise e
+          else
+            @failed.delete("#{@options[:host]}:#{@options[:port]}") if @failed
+          end
         end
       else
         connect_without_sentinel
@@ -18,9 +29,6 @@ module RedisWmrs
     end
 
     def discover_slave
-      @my_hostname = Socket::gethostname rescue nil
-      @my_ip = IPSocket::getaddress(@my_hostname) rescue nil
-
       while true
         try_next_sentinel
         host_attrs = fetch_slaves
@@ -55,21 +63,37 @@ module RedisWmrs
         {"ip" => h["master-host"], "port" => h["master-port"], "slave-priority" => MASTER_PRIORITY}
       }.uniq
       attrs_array = slave_attrs_array + master_attrs_array
-      [@my_ip, @my_hostname, "127.0.0.1", "localhost"].compact.each do |ip_hostname|
+      [*self.class.ip_and_hostnames, "127.0.0.1", "localhost"].compact.each do |ip_hostname|
         if idx = attrs_array.index{|attrs| attrs["ip"] ==  ip_hostname}
           preferred_indexes << idx
         end
       end
+      # slave-priorityの値が小さいほどmasterに昇格しやすいので、
       # preferredではないslaves群についてはslave-priorityの降順で接続の優先順位を振ります。
       not_preferred = ((0...attrs_array.length).to_a - preferred_indexes).
         sort_by{|i| attrs_array[i]["slave-priority"].to_i}.reverse
       result = (preferred_indexes + not_preferred).map{|i| attrs_array[i]}
+      # 接続に失敗した接続先は優先度を下げます
+      puts "@failed: #{@failed.inspect}"
+      (@failed || []).each do |failed|
+        if idx = result.index{|r| failed == "#{r['ip']}:#{r['port']}" }
+          result.push(result.delete_at(idx))
+        end
+      end
       return result
     rescue Exception => e
-      # puts "[#{e.class}] #{e.message}\n  " << e.backtrace.join("\n  ")
+      puts "[#{e.class}] #{e.message}\n  " << e.backtrace.join("\n  ")
       raise e
     end
     private :fetch_slaves
+
+    def self.ip_and_hostnames
+      unless @my_hostname
+        @my_hostname = Socket::gethostname rescue nil
+        @my_ip = IPSocket::getaddress(@my_hostname) rescue nil
+      end
+      return @my_hostname, @my_ip
+    end
 
   end
 end
